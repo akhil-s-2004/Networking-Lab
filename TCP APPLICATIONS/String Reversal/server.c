59c0dc52e6ac7b2b2122e43dc042e71ac9c11942
AKHIL S NAIR <akhilsnair2004@gmail.com>
2025-04-12 14:11:28 +0530

    Create server.c

---
+++ TCP APPLICATIONS/String Reversal/server.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+
+#define PORT 1312
+#define BUFFER_SIZE 1024
+
+void reverse(char buffer[]){
+	int i,j,length;
+	char temp;
+	length=strlen(buffer);
+	for(i=0,j=length-2;i<=j;i++,j--){
+		temp=buffer[i];
+		buffer[i]=buffer[j];
+		buffer[j]=temp;
+	}
+}
+int main(){
+	int check,server_socket,client_socket;
+	struct sockaddr_in server_addr,client_addr;
+	socklen_t addr_size;
+	char buffer[BUFFER_SIZE];
+	server_socket=socket(AF_INET,SOCK_STREAM,0);
+	if(server_socket==-1){
+		perror("Socket creation failed!!\n");
+		exit(1);
+	}
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_addr.s_addr=inet_addr("127.0.0.12");
+	server_addr.sin_port=htons(PORT);
+	if(bind(server_socket,(struct sockaddr*)&server_addr,sizeof(server_addr))<0)
+	{
+		perror("Binding failed!!");
+		exit(1);
+	}
+	if(listen(server_socket,1)<0){
+		perror("Listening failed!!!\n");
+		exit(1);
+	}
+	printf("Server is listening on port %d\n",PORT);
+	addr_size=sizeof(client_addr);
+	client_socket=accept(server_socket,(struct sockaddr*)&client_addr,&addr_size);
+	if(client_socket<0)
+	{
+		perror("ACCEPT FAILED!!\n");
+		exit(1);
+	}
+	while(1){
+		memset(buffer,0,BUFFER_SIZE);
+		if(recv(client_socket,buffer,BUFFER_SIZE,0)<=0){
+			printf("Client disconnected!!\n");
+			break;
+		}
+		printf("Client: %s",buffer);
+		reverse(buffer);
+		printf("Server: %s",buffer);
+		send(client_socket,buffer,strlen(buffer),0);
+	}
+	close(client_socket);
+	close(server_socket);
+}
